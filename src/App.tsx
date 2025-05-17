63d5845562f1652c1602a2f5c257202b21bdbe95
Mike Lynagh <mlynagh111@gmail.com>
2025-05-17 19:16:03 +0100

    chore: initial setup with Vite, React, TS, tailwind and basic folder setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center font-fira text-white">
+      <h1>Hello Genesys</h1>
+
+    </div>
+
+  )
+}
+
+export default App
