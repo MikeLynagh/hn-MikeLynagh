27d2934efe5349c004e577c7ff3c9896a069c726
Mike Lynagh <mlynagh111@gmail.com>
2025-05-18 09:41:15 +0100

    feat: added footer and header

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+    return (
+        <footer className="h-32 mt-20 sm:mt-36text-lg font-semibold text-gray-400 flex flex-col-reverse sm:flex-row items-center
+        justify-center sm:justify-around">
+            <p>Code and Design by</p>
+            <a href="https://mikelynagh.com" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-500 border-b border-dotted hover:border solid 
+            // hover:border-yellow-500 border-gray-400">Mike Lynagh</a>
+        
+        </footer>
+    )
+}
+
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <header className="max-w-2xl mx-auto p-4">
+        <h1 className="text-4xl sm:text-6xl font-black text-center sm:text-left text-transparent 
+            bg-clip-text bg-gradient-to-r from-yellow-500 via-pink-500 to-red-500">Hacker News
+            </h1>
+        </header>
+    )
+}
